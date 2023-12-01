b99239bf7cf50e26ddae1b7f0d837521b77e9c26
sufyanali <sufyanalimir313@gmail.com>
2023-12-01 20:53:24 +0530

    converted to hooks and deployed final project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import "./App.css";
-import Navigation from "./components/Navigation/Navigation";
+// import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import InputForm from "./components/InputForm/InputForm";
-import Rank from "./components/Rank/Rank";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
-import Register from "./components/Register/Register";
+// import Register from "./components/Register/Register";
 import ParticlesBg from "particles-bg";
-import { Component } from "react";
-import SignIn from "./components/SignIn/SignIn";
+import { useState } from "react";
+// import SignIn from "./components/SignIn/SignIn";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      input: "",
-      imageUrl: "",
-      box: {},
-      route: "signin",
-      isSignedIn: false,
-    };
-  }
+function App() {
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [boxArray, setBoxArray] = useState([]);
+  // const [route, setRoute] = useState("signin");
+  // const [isSignedIn, setIsSignedIn] = useState(false);
 
-  calculateFaceLocation = (data) => {
+  console.log(boxArray);
+
+  const calculateFaceLocation = (data) => {
     const clarifai_face = data;
     const input_img = document.getElementById("inputImg");
     const img_width = Number(input_img.width);
@@ -35,17 +31,11 @@ class App extends Component {
     };
   };
 
-  displayBox = (box) => {
-    console.log(box);
-    this.setState({ box: box });
-    console.log(box);
+  const onInputChange = (event) => {
+    setInput(event.target.value);
   };
 
-  onInputChange = (event) => {
-    this.setState({ input: event.target.value });
-  };
-
-  returnClarifaiRequestOptions = (imageUrl) => {
+  const returnClarifaiRequestOptions = (imageUrl) => {
     const PAT = "8c0dc78237c24c6cbf014a06d03abb3d";
     const USER_ID = "sufyanalimir";
     const APP_ID = "faceApp";
@@ -78,73 +68,55 @@ class App extends Component {
     return requestOptions;
   };
 
-  onBtnSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
+  const onBtnSubmit = () => {
+    setImageUrl(input);
     fetch(
       "https://api.clarifai.com/v2/models/face-detection/outputs",
-      this.returnClarifaiRequestOptions(this.state.input)
+      returnClarifaiRequestOptions(input)
     )
       .then((response) => response.json())
       .then((result) => {
-        if (result.outputs.length === 1) {
-          this.displayBox(
-            this.calculateFaceLocation(
-              result.outputs[0].data.regions[0].region_info.bounding_box
-            )
-          );
-        } else {
-          result.outputs.forEach((output) => {
-            output.data.regions.forEach((region) => {
-              this.displayBox(
-                this.calculateFaceLocation(region.region_info.bounding_box)
-              );
-            });
-          });
-        }
+        const boxes = result.outputs.flatMap((output) =>
+          output.data.regions.map((region) =>
+            calculateFaceLocation(region.region_info.bounding_box)
+          )
+        );
+        setBoxArray(boxes);
       })
       .catch((error) => console.log("error", error));
   };
 
-  onRouteChange = (route) => {
-    if (route === "signout") {
-      this.setState({ isSignedIn: false });
-    } else if (route === "home") {
-      this.setState({ isSignedIn: true });
-    }
-    this.setState({ route: route });
-  };
+  // const onRouteChange = (route) => {
+  //   if (route === "signout") {
+  //     setIsSignedIn(false);
+  //   } else if (route === "home") {
+  //     setIsSignedIn(true);
+  //   }
+  //   setRoute(route);
+  // };
 
-  render() {
-    return (
-      <div className="App">
-        <ParticlesBg color="#333333" num={60} type="cobweb" bg={true} />
-        <div className="flex justify-between pa3">
-          <Logo />
-          <Navigation
-            onRouteChange={this.onRouteChange}
-            isSignedIn={this.state.isSignedIn}
-          />
-        </div>
-        {this.state.route === "home" ? (
-          <>
-            {/* <Rank /> */}
-            <InputForm
-              onInputChange={this.onInputChange}
-              onBtnSubmit={this.onBtnSubmit}
-            />
-            <FaceRecognition
-              box={this.state.box}
-              imageUrl={this.state.imageUrl}
-            />
-          </>
-        ) : this.state.route === "register" ? (
-          <Register onRouteChange={this.onRouteChange} />
-        ) : (
-          <SignIn onRouteChange={this.onRouteChange} />
-        )}
+  return (
+    <div className="App">
+      <ParticlesBg color="#333333" num={60} type="cobweb" bg={true} />
+      <div className="flex justify-between pa3">
+        <Logo />
+        {/* <Navigation onRouteChange={onRouteChange} isSignedIn={isSignedIn} /> */}
       </div>
-    );
-  }
+      <InputForm onInputChange={onInputChange} onBtnSubmit={onBtnSubmit} />
+      <FaceRecognition boxArray={boxArray} imageUrl={imageUrl} />
+
+      {/* {route === "home" ? (
+        <>
+          <InputForm onInputChange={onInputChange} onBtnSubmit={onBtnSubmit} />
+          <FaceRecognition boxArray={boxArray} imageUrl={imageUrl} />
+        </>
+      ) : route === "register" ? (
+        <Register onRouteChange={onRouteChange} />
+      ) : (
+        <SignIn onRouteChange={onRouteChange} />
+      )} */}
+    </div>
+  );
 }
 
 export default App;
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,20 +1,21 @@
 import React from "react";
 
-const FaceRecognition = ({ imageUrl, box }) => {
-  //console.log(box);
+const FaceRecognition = ({ imageUrl, boxArray }) => {
   return (
     <div className="center ma3">
       <div className="absolute mt2" style={{ top: 285 }}>
         <img id="inputImg" src={imageUrl} alt="" />
-        <div
-          className="bounding_box"
-          style={{
-            top: box.topRow,
-            right: box.rightCol,
-            left: box.leftCol,
-            bottom: box.bottomRow,
-          }}
-        ></div>
+        {boxArray.map((box) => (
+          <div
+            className="bounding_box"
+            style={{
+              top: box.topRow,
+              right: box.rightCol,
+              left: box.leftCol,
+              bottom: box.bottomRow,
+            }}
+          ></div>
+        ))}
       </div>
     </div>
   );
